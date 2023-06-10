9d34a6116906a8f06fc4c482e02db90cf5df0075
Dipansu Rout <dip9381@gmail.com>
2023-06-10 21:41:06 +0530

    commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,69 @@
-import logo from './logo.svg';
-import './App.css';
-
+import { useEffect, useState } from "react";
+import axios from "axios";
+import "./style.css"
 function App() {
+  const [data,setdata]=useState("");
+  useEffect(()=>{
+    axios.get('https://dummyjson.com/users')
+    .then((res)=>{
+      setdata(res.data.users);
+    })
+    .catch((err)=>{
+      console.log(err);
+    })
+  },[])
+  useEffect(()=>{
+    console.log(data);
+  },[data])
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    <table>
+      <tr>
+        <th>id</th>
+        <th>firstName</th>
+        <th>lastName</th>
+        <th>age</th>
+        <th>gender</th>
+        <th>email</th>
+        <th>phone</th>
+        <th>username</th>
+        <th>birthDate</th>
+        <th>image</th>
+        <th>height</th>
+        <th>weight</th>
+        <th>address</th>
+        <th>bank</th>
+        <th>company</th>
+      </tr>
+      {
+        data.map((val)=>{
+          return(
+            <>
+            <tr>
+              <td>{val.id}</td>
+              <td>{val.firstName}</td>
+              <td>{val.lastName}</td>
+              <td>{val.age}</td>
+              <td>{val.gender}</td>
+              <td>{val.email}</td>
+              <td>{val.phone}</td>
+              <td>{val.username}</td>
+              <td>{val.birthDate}</td>
+              <td><img src={val.image} alt="" srcset="" /></td>
+              <td>{val.height}</td>
+              <td>{val.weight}</td>
+              <td>city: {val.address.city} <br />
+              coord: {val.address.coordinates.lat} {val.address.coordinates.lng}
+              </td>
+              <td>cardExpire: {val.bank.cardExpire}</td>
+              <td>{val.company.name}</td>
+            </tr>
+            </>
+          );
+        })
+      }
+    </table>
+    </>
   );
 }
 
